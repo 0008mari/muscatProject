fe5bffa15de5fa7d9ffe3172bd831617fd179257
0008mari <0008mari@gmail.com>
2022-03-08 22:54:22 +0900

    making crud

---
+++ server/muscatApi/muscatApi/Models/MyUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace muscatApi.Models
+{
+    [Table("MyUser")]
+    public partial class MyUser
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(60)]
+        [Unicode(false)]
+        public string Email { get; set; } = null!;
+        [Column(TypeName = "date")]
+        public DateTime BirthDate { get; set; }
+        [StringLength(10)]
+        public string Name { get; set; } = null!;
+        public int MyStatus { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime RegisteredDate { get; set; }
+
+        [InverseProperty("IdNavigation")]
+        public virtual Pwd Pwd { get; set; } = null!;
+    }
+}
---
+++ server/muscatApi/muscatApi/Services/UserService.cs
@@ -0,0 +1,62 @@
+﻿using muscatApi.Models;
+using muscatApi.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace muscatApi.Services
+{
+    public class UserService
+    {
+        private muscatApiContext _context;
+
+        public UserService(muscatApiContext context)
+        {
+            _context = context;
+        }
+
+        // CRUD operation
+
+        public List<MyUser> GetAll()
+        {
+            return _context.MyUsers
+                .ToList();
+        }
+        
+        public MyUser? GetById(int id)
+        {
+            return _context.MyUsers
+                .Include(p => p.Id)
+                .Include(p => p.Email)
+                .Include(p => p.Name)
+                .SingleOrDefault(p => p.Id == id);
+        }
+
+        public MyUser Create(MyUser newUser)
+        {
+            _context.MyUsers.Add(newUser);
+            _context.SaveChanges();
+
+            return newUser;
+        }
+
+        // UpdatePassword-  아직 미구현
+        
+        public bool DeleteById(int id)
+        {
+            var userToDelete = _context.MyUsers.Find(id);
+            if (userToDelete != null)
+            {
+                _context.MyUsers.Remove(userToDelete);
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+   
+}
